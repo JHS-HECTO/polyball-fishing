4fbf877ba2f11cd167b81736f2952fc9c08a1366
wjdghltjd77-hue <wjdghltjd77@gmail.com>
2026-05-15 14:06:54 +0900

    feat: add cast button

---
+++ components/CastButton.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import clsx from 'clsx';
+import styles from './CastButton.module.scss';
+
+type Props = {
+  onClick: () => void;
+  disabled?: boolean;
+  label?: string;
+};
+
+export function CastButton({ onClick, disabled = false, label = '캐스팅!' }: Props) {
+  return (
+    <button
+      className={clsx(styles.cast, { [styles['cast--disabled'] as string]: disabled })}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {label}
+    </button>
+  );
+}
